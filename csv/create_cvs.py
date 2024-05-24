178f119a36f684320134b8fdd76fbc0baebc8fa0
VladSkopenko <skopirka2k17@gmail.com>
2024-05-24 20:53:03 +0300

    create cvs with py

---
+++ csv/create_cvs.py
@@ -0,0 +1,18 @@
+import csv
+
+
+data = [
+    ["name", "email", "phone", "favorite"],
+    ["Allen Raymond", "nulla.ante@vestibul.co.uk", "(992) 914-3792", "False"],
+    ["Chaim Lewis", "dui.in@egetlacus.ca", "(294) 840-6685", "False"],
+    ["Kennedy Lane", "mattis.Cras@nonenimMauris.net", "(542) 451-7038", "True"],
+    ["Wylie Pope", "est@utquamvel.net", "(692) 802-2949", "False"],
+    ["Cyrus Jackson", "nibh@semsempererat.com", "(501) 472-5218", "True"]
+]
+
+# Створення та запис у файл contacts.csv
+with open('user.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
+
+print("CSV файл успішно створено.")
