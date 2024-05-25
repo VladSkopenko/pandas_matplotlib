99c4d1e6a14948bb976c357e46aafcf6e7905eb1
VladSkopenko <skopirka2k17@gmail.com>
2024-05-25 16:47:14 +0300

    add read_json with mode split and records and lines=True

--- json/pydantic_json.py
+++ json/pydantic_json.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
 print("Что бы прочитать 1 обьект json - lines=True")
-user = pd.read_json("user.json", lines=True)
+user = pd.read_json("user.json", lines=True, )
 print(user)
 print("--" * 20)
 print("Что бы прочитать полноценный json  [{},{},{}] существует метод read_json()")
 users = pd.read_json("users.json")
 print(users)
+print("--" * 20)
+print("Режим рекорд когда КЛЮЧ:ЗНАЧЕНИЕ")
+users = pd.read_json("users.json", orient="records")
+print(users)
+print("--" * 20)
+print("Режим сплит нужен когда структура КЛЮЧ:СПИСОК")
+employees = pd.read_json("empl.json", orient="split")
+print(employees)
