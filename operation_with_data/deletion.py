543d949a353cf7eb1df1fcbab23904e2b61f7639
VladSkopenko <skopirka2k17@gmail.com>
2024-05-26 13:59:41 +0300

    axis=0 axis=1

--- operation_with_data/deletion.py
+++ operation_with_data/deletion.py
@@ -5,7 +5,8 @@ numbers = pd.Series([1, 2, 3, 4, 5])
 numbers.drop([1, 3], inplace=True)
 print(numbers)
 print("--" * 20)
-print("З датафрейма тоже самое можна виконувати. pd.DataFrame.drop([index1, index2, ...])")
+print("З датафрейма тоже самое можна виконувати. pd.DataFrame.drop([index1, index2, ...]),"
+      " axis=0 - для строк, axis=1 - для стовпців")
 data = {
     "name": {"1": "Michael", "2": "John", "3": "Liza"},
     "country": {"1": "Canada", "2": "USA", "3": "Australia"},
@@ -13,5 +14,6 @@ data = {
 }
 
 employees = pd.DataFrame(data)
-employees = employees.drop(["2"])
+employees = employees.drop(["2"], axis=0)
+employees = employees.drop(["age"], axis=1)
 print(employees)
