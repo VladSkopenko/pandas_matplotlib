a5475561608b2126f6087fa4dd2d7be099e38732
VladSkopenko <skopirka2k17@gmail.com>
2024-05-26 13:57:58 +0300

    pd.Object.drop(

---
+++ operation_with_data/deletion.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+print("Видалення елементів по індексу. pd.Series.drop([index1, index2, ...])")
+numbers = pd.Series([1, 2, 3, 4, 5])
+numbers.drop([1, 3], inplace=True)
+print(numbers)
+print("--" * 20)
+print("З датафрейма тоже самое можна виконувати. pd.DataFrame.drop([index1, index2, ...])")
+data = {
+    "name": {"1": "Michael", "2": "John", "3": "Liza"},
+    "country": {"1": "Canada", "2": "USA", "3": "Australia"},
+    "age": {"1": 25, "2": 32, "3": 19}
+}
+
+employees = pd.DataFrame(data)
+employees = employees.drop(["2"])
+print(employees)
