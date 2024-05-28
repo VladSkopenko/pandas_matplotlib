9981573a1f1810dea026ce1efac234524705200d
VladSkopenko <skopirka2k17@gmail.com>
2024-05-28 16:36:43 +0300

    Add linestyle and color

--- visualization/сomposition_plots.py
+++ visualization/сomposition_plots.py
@@ -7,9 +7,9 @@ fig, axs = plt.subplots()  # создает несколько графиков 
 night_temp = [15, 18, 14, 15, 18, 16, 19, 17]
 day_temp = [21, 20, 22, 21, 24, 21, 25, 21]
 mid_temp = [(x + y) / 2 for x, y in zip(night_temp, day_temp)]
-axs.plot(date, night_temp, label="Нічна темпетатура")  # добавляет график
-axs.plot(date, day_temp, label="Денна темпетатура")  # добавляет график
-axs.plot(date, mid_temp, label="Середня темпетатура")  # добавляет график
+axs.plot(date, night_temp, label="Нічна темпетатура", color="blue", linestyle="--")  # добавляет график
+axs.plot(date, day_temp, label="Денна темпетатура", color="red", linestyle=":")  # добавляет график
+axs.plot(date, mid_temp, label="Середня темпетатура", color="green", linestyle="-.")  # добавляет график
 
 plt.xlabel("Дата", fontsize=14,
            color="black", loc="center",
