0bf8582cd22d16d15a95902a4499829a94f5d5c6
VladSkopenko <skopirka2k17@gmail.com>
2024-05-29 15:49:57 +0300

    add funch barplot and pointplot

---
+++ visualization/seaborn/Visualization_of_categorical_data_assessments/funch_bar_plot.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = sns.load_dataset("mpg")
+
+sns.barplot(x="origin", y="mpg", data=data)
+"""Функція будує стовпчасту діаграму: висота бару (стовпця) визначає чисельне значення оцінки ознаки
+ (математичне очікування), лінія, що перетинає верхню межу бару - довірчий інтервал."""
+
+plt.show()
---
+++ visualization/seaborn/Visualization_of_categorical_data_assessments/funk_poitn_plot.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+data = sns.load_dataset("mpg")
+
+
+sns.pointplot(x="origin", y="mpg", data=data)
+#Відображає оцінку будь-якого набору даних як точку на полі графіка та довірчий інтервал у вигляді лінії, центр якої лежить на зазначеній точці.
+
+plt.show()
+
+
