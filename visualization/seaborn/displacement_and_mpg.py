1f2528b85f04c0e443a27b4b70b2ae5e20eebca5
VladSkopenko <skopirka2k17@gmail.com>
2024-05-29 15:23:48 +0300

    seaborn point-plot

--- visualization/seaborn/displacement_and_mpg.py
+++ visualization/seaborn/displacement_and_mpg.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 
 data = sns.load_dataset("mpg")  # это просто берем с интернета
 
-sns.scatterplot(x='mpg', y='displacement', data=data)
+sns.scatterplot(x='mpg', y='displacement', data=data, hue="origin")
 
 
 
 plt.xlabel("Витрати палива")
-plt.ylabel("Об'єм двигуна")
-plt.title("Залежність витрат палива від об'єму двигуна")
+plt.ylabel("Дальність пробігу")
+plt.title("Залежність витрат палива від дальності пробігу")
 
 plt.show()
